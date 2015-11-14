d79221bc584a35e64eb782b162bc1b17ee6a03c0
Quinn McHenry <quinn@jqm.us>
2015-11-14 10:13:57 -0500

    Initial Commit

---
+++ Saturn/Saturn.h
@@ -0,0 +1,19 @@
+//
+//  Saturn.h
+//  Saturn
+//
+//  Created by Quinn McHenry on 11/14/15.
+//  Copyright © 2015 Small Planet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Saturn.
+FOUNDATION_EXPORT double SaturnVersionNumber;
+
+//! Project version string for Saturn.
+FOUNDATION_EXPORT const unsigned char SaturnVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Saturn/PublicHeader.h>
+
+
